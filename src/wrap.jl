fce11f80eed90c30fe6fb0531ec434e72d8960a7
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-15 15:01:13 -0500

    updated wrap/unwrap

--- src/wrap.jl
+++ src/wrap.jl
@@ -1,15 +1,27 @@
-function wrap!(box, coords, images=nothing)
+"""
+    wrap!(coords, box)
+
+Wrap molecular coordinates into a box with bounds
+`box = [xlo xhi; ylo yhi; zlo zhi]`. Returns the resulting image values,
+a Matrix with the same size as `coords`.
+"""
+function wrap!(coords, box)
     boxlo, boxhi = box[ : , 1], box[ : , 2]
     L = boxhi - boxlo
-    if images !== nothing
-        images += fld.(coords .- boxlo, L)
-    end
+    images = fld.(coords .- boxlo, L)
     coords = ((coords .- boxlo) .% L) .+ boxlo
-    return nothing
+    return images
 end
 
-function unwrap!(box, coords, images, bonds=nothing)  # Not yet implemented
-    boxlo, boxhi = box[ : , 1], box[ : , 2]
-    L = boxhi - boxlo
+"""
+    unwrap!(coords, box, images)
+
+Unwrap molecular coordinates from a box with bounds
+`box = [xlo xhi; ylo yhi; zlo zhi]` and image values given by `images`,
+a Matrix with the same size as `coords`.
+"""
+function unwrap!(coords, box, images)
+    L = box[ : , 2] - box[ : , 1]
+    coords += L .* images
     return nothing
 end
